fb726803e6738b1860d2c0b1e6b15fab31e1955e
Jackson Gariety <personal@jacksongariety.com>
2013-09-04 03:45:27 -0700

    Adds a few utilities I hacked together with @benjamesmartin.

---
+++ wunderscore.js
@@ -0,0 +1,77 @@
+function each(collection, iterator) {
+  if (Array.isArray(collection)) {
+    var i = collection.length
+    while (i--) {
+      iterator(collection[i])
+    }
+  } else {
+    for (var prop in collection) {
+      iterator(collection.prop)
+    }
+  }
+}
+
+function indexOf(array, value) {
+  if (Array.indexOf) {
+    return array.indexOf(value)
+  } else {
+    for (var i = 0; i < array.length; i++) if (array[i] == value) return i
+    return -1
+  }
+}
+
+function filter(collection, iterator) {
+  if (Array.isArray(collection)) {
+    var newCollection = new Array()
+    for (var i = 0; i < collection.length; i++) iterator(collection[i]) && newCollection.push(collection[i])
+  } else {
+    var newCollection = new Object()
+    for (var i in collection) if (iterator(collection[i])) newCollection[i] = collection[i]
+  }
+    
+  return newCollection
+}
+
+function reject(collection, iterator) {
+  var newArray = new Array()
+
+  if (Array.isArray(collection)) {
+    var newCollection = new Array()
+    for (var i = 0; i < collection.length; i++) !iterator(collection[i]) && newCollection.push(collection[i])
+  } else {
+    var newCollection = new Object()
+    for (var i in collection) if (!iterator(collection[i])) newCollection[i] = collection[i]
+  }
+    
+  return newCollection
+}
+
+function uniq(array) {
+  var newArray = new Array()
+  for (var i = 0; i < array.length; i++) (indexOf(newArray, array[i]) == -1)  && newArray.push(array[i])
+  return newArray
+}
+
+function map(collection, iterator) {
+  if (Array.isArray(collection)) {
+    var newCollection = new Array()
+    for (var i = 0; i < collection.length; i++) newCollection.push(iterator(collection[i]))
+  } else {
+    var newCollection = new Object()
+    for (var i in collection) newCollection[i] = iterator(collection[i])
+  }
+    
+  return newCollection
+}
+
+function invoke(collection, methodName) {
+  return map(collection, function(value) {
+    return (typeof methodName == 'function' ? methodName : value[methodName]).apply(value)
+  })
+}
+
+function typeOf(object) {
+  if (object instanceof Array) { return 'array' }
+  else if (object == null) { return 'null' }
+  else { return typeof object }
+}
